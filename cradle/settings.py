51965665fe9d8b379799db2d7eb937bb521a456d
yth <yuting2727@gmail.com>
2023-07-28 16:33:05 +0000

    Add interactive frontend
    Ref: https://github.com/victordonoso/chatgpt_clone/blob/master/openai_django/templates/query.html

--- cradle/settings.py
+++ cradle/settings.py
@@ -53,7 +53,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    
+    # local apps
     'linechatbot.apps.LinechatbotConfig',
     'petlover.apps.PetloverConfig',
     "frontend.apps.FrontendConfig",
--- petlover/views.py
+++ petlover/views.py
@@ -15,16 +15,16 @@ gpt_task_assigner = GPTTaskAssigner(settings.OPENAI_API_KEY, pfj_src_dict=settin
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
-        event = json.loads(request.body.decode('utf-8'))
-        txt = event['txt']
-        response_dict = {}
-        if '%petfriendly%' in txt:
-            reply = gpt_task_assigner.judge_store_pet_friendly(txt)  
+        #event = json.loads(request.body.decode('utf-8'))
+        prompt = request.POST.get('prompt')
+        if '%petfriendly%' in prompt:
+            reply = gpt_task_assigner.judge_store_pet_friendly(prompt)  
         else:
-            reply = gpt_task_assigner.chat(txt) 
+            reply = gpt_task_assigner.chat(prompt) 
         
-        response_dict['reply'] = reply
         
-        return JsonResponse(response_dict)
+        return JsonResponse({
+            'response': reply,
+            })
     else:
         return HttpResponseBadRequest()
