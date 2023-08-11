656fe2cfbb571853d301c2eaa57ed1c138a3462d
hyt <yuting2727@gmail.com>
2023-08-11 18:10:30 +0000

    add return customized store info

--- petlover/views.py
+++ petlover/views.py
@@ -12,51 +12,69 @@ from .pet_friendly.src.tools import get_file_contents
 
 gpt_task_assigner = GPTTaskAssigner(settings.OPENAI_API_KEY, pfj_src_dict=settings.PET_FRIENDLY_JUDGER_SRC_DICT)
 
+def _generate_response(place_name, reply):
+    """
+    This is a funny way (but the only way so far) to make it stream-like from client side.
+    We must return meaningless \n in an interweave manner; 
+    otherwise it will not pop up the next thing until it get to the end.
+    
+    """
+    cnt = 0
+    while True:
+        try:
+            if cnt % 2 ==0:
+                if cnt == 0:
+                    yield f"Is {place_name} pet-friendly?"
+                else:
+                    yield "\n"
+            else:
+                chunk = next(reply)
+                content = chunk["choices"][0].get("delta", {}).get("content")
+                if content is not None:
+                    #print(content)
+                    yield content
+
+        except StopIteration:
+            break 
+        cnt+=1
+
+def check_is_cradle_confirm():
+    #TODO(kt): re-load pandas every time for now 
+    #cradle_db = pd.read_csv(settings.cradle_database_path)
+
+    # url 2 lat lng
+    return False
+
 @csrf_exempt
 def callback(request):
     if request.method == 'POST':
         if_stream = True
         post_data = json.loads(request.body.decode("utf-8"))
-        txt = post_data['txt']
-        print(f'User posting: {txt}')
-        if '%petfriendly%' in txt:
-            txt = txt.split("friendly%")[-1]
+        api_input = post_data['api_input']
+        print(f'Front-end posting: {api_input}')
+        if '%petfriendly%' in api_input:
+            place_id = api_input.split("friendly%")[-1]
             print("Judging pet-friendly...")
-            place_name, reply = gpt_task_assigner.judge_store_pet_friendly(txt, if_stream=if_stream)  
+            
+            if check_is_cradle_confirm():
+                reply = {
+                        "confirm_date": "2023-08-10",
+                        "manager": "KT",
+                        "service_dog": True,
+                        "non_service_dog": True,
+                        "dog_treat": True,
+                        "dog_water": False,
+                        }
+                return JsonResponse({
+                    'response': reply,
+                    })
+            else:
+                place_name, reply = gpt_task_assigner.judge_store_pet_friendly(place_id, if_stream=if_stream) 
+                # Return a streaming response to the client
+                print("Gpt streaming...") 
+                return StreamingHttpResponse(_generate_response(place_name, reply), content_type='text/event-stream')
+
         else:
             reply = gpt_task_assigner.chat(txt) 
-        print("Gpt streaming...") 
-        if if_stream:
-            def _generate_response():
-                """
-                This is a funny way (but the only way so far) to make it stream-like from client side.
-                We must return meaningless \n in an interweave manner; 
-                otherwise it will not pop up the next thing until it get to the end.
-                
-                """
-                cnt = 0
-                while True:
-                    try:
-                        if cnt % 2 ==0:
-                            if cnt == 0:
-                                yield f"Is {place_name} pet-friendly?"
-                            else:
-                                yield "\n"
-                        else:
-                            chunk = next(reply)
-                            content = chunk["choices"][0].get("delta", {}).get("content")
-                            if content is not None:
-                                #print(content)
-                                yield content
-
-                    except StopIteration:
-                        break 
-                    cnt+=1
-            # Return a streaming response to the client
-            return StreamingHttpResponse(_generate_response(), content_type='text/event-stream')
-        else:
-            return JsonResponse({
-                'response': reply,
-                })
     else:
         return HttpResponseBadRequest()
